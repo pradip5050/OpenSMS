553ffea4f129becfb32ea8613c027741d6a8f825
dhzdhd <ragas5432100@gmail.com>
2024-05-20 11:52:13 +0530

    Add zod and react-hook-forms dep

--- frontend/src/components/login/LoginForm.tsx
+++ frontend/src/components/login/LoginForm.tsx
@@ -17,7 +17,7 @@ export function LoginForm() {
 
   return (
     <div className="flex items-center justify-center py-12">
-      <div className="mx-auto grid w-[350px] gap-6">
+      <form className="mx-auto grid w-[350px] gap-6">
         <div className="grid gap-2 text-center">
           <h1 className="text-3xl font-bold">Login</h1>
           <p className="text-balance text-muted-foreground"></p>
@@ -51,7 +51,7 @@ export function LoginForm() {
             <LoadCanvasTemplateNoReload />
           </div> */}
         </div>
-      </div>
+      </form>
     </div>
   );
 }
