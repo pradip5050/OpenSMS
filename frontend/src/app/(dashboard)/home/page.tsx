168ae2ce5f42fb96075accfb47629c407ee7c3ec
dhzdhd <ragas5432100@gmail.com>
2024-05-23 12:14:36 +0530

    Add sheet navbar drawer for small devices

--- frontend/src/app/(dashboard)/home/page.tsx
+++ frontend/src/app/(dashboard)/home/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
+    <main className="min-h-screen p-4 flex flex-col items-center justify-center">
       Home
     </main>
   );
--- frontend/src/components/dashboard/Navbar.tsx
+++ frontend/src/components/dashboard/Navbar.tsx
@@ -27,27 +27,25 @@ export default function Navbar() {
   }, [currentPath]);
 
   return (
-    <aside className="hidden md:flex flex-col w-48 mt-16 border border-t-0 py-2">
-      <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-        {routes.map((route: Route) => {
-          return (
-            <Link
-              key={route.name}
-              href={route.path}
-              className={cn([
-                "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
-                route.selected ? "bg-muted" : "",
-              ])}
-            >
-              <Home className="h-4 w-4" />
-              {route.name}
-              {/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+    <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+      {routes.map((route: Route) => {
+        return (
+          <Link
+            key={route.name}
+            href={route.path}
+            className={cn([
+              "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+              route.selected ? "bg-muted" : "",
+            ])}
+          >
+            <Home className="h-4 w-4" />
+            {route.name}
+            {/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                 6
               </Badge> */}
-            </Link>
-          );
-        })}
-      </nav>
-    </aside>
+          </Link>
+        );
+      })}
+    </nav>
   );
 }
