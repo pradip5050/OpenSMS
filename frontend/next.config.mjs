3aff7a67a6f553004b837a744ec712461accff05
dhzdhd <ragas5432100@gmail.com>
2024-05-28 02:12:42 +0530

    Revert dev config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
-const isUsingTauri = true;
+const isUsingTauri = false;
 
 let internalHost = null;
 
