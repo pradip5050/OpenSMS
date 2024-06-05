ef0bba342afebc2b6596b28dd06918f5329d4d3a
dhzdhd <ragas5432100@gmail.com>
2024-06-05 11:53:38 +0530

    Implement course fetching and update UI

--- frontend/src/lib/dashboard/courses.ts
+++ frontend/src/lib/dashboard/courses.ts
@@ -1,6 +1,42 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { GetResponse } from "../utils";
+import useSWR from "swr";
+import { API_URL } from "../constants";
+
 export interface Course {
   id: string;
   code: string;
   name: string;
   credits: number;
+}
+export interface CourseResponse {
+  docs?: Course[];
+}
+
+export function useCourses(
+  token?: string
+): GetResponse<CourseResponse | undefined> {
+  const { data, error, isLoading } = useSWR<CourseResponse, AxiosError>(
+    `${API_URL}/api/courses?draft=false&depth=2`,
+    (url: string) =>
+      axios
+        .get(url, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res: AxiosResponse<CourseResponse>) => res.data)
+  );
+
+  //   const transformedData: AttendanceResponse | undefined = {
+  //     docs: data?.docs?.map((fee) => {
+  //       return { ...fee, date: new Date(fee.date) };
+  //     }),
+  //   };
+
+  return {
+    data,
+    error,
+    isLoading,
+  } satisfies GetResponse<CourseResponse | undefined>;
 }
