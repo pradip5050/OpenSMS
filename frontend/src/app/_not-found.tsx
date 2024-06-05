3eb1403a33f0555614bb84c3333f30fe9278bb76
dhzdhd <ragas5432100@gmail.com>
2024-06-05 19:38:28 +0530

    Add dummy error routes

---
+++ frontend/src/app/_not-found.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import "./globals.css";
+import { useRouter } from "next/navigation";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+// TODO: Implement when https://github.com/vercel/next.js/discussions/50034 is fixed
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return (
+    <ThemeProvider
+    //   attribute="class"
+    //   defaultTheme="system"
+    // enableSystem
+    //   disableTransitionOnChange
+    >
+      <div className="h-screen w-screen flex flex-col gap-5 items-center justify-center bg-background text-foreground">
+        <h1 className="text-5xl">404 Not Found</h1>
+        <Button onClick={() => router.push("/home")}>Return Home</Button>
+      </div>
+    </ThemeProvider>
+  );
+}
--- frontend/src/components/AuthError.tsx
+++ frontend/src/components/AuthError.tsx
@@ -6,7 +6,7 @@ export default function AuthError() {
   const router = useRouter();
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
       <h1 className="text-5xl">Error!</h1>
       <h2 className="text-3xl py-5">Not Authenticated</h2>
       <Button onClick={() => router.push("/")} className="mt-3">
