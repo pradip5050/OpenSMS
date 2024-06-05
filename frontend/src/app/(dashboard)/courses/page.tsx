a082e14e8c372074af0d45097df6b56234e54f41
dhzdhd <ragas5432100@gmail.com>
2024-06-05 12:10:56 +0530

    Add data table and implement it in courses page

--- frontend/src/app/(dashboard)/courses/page.tsx
+++ frontend/src/app/(dashboard)/courses/page.tsx
@@ -1,12 +1,49 @@
 "use client";
 
 import { useAuth } from "@/components/AuthProvider";
+import { DataTable } from "@/components/dashboard/DataTable";
 import Spinner from "@/components/Spinner";
-import { useCourses } from "@/lib/dashboard/courses";
+import { Button } from "@/components/ui/button";
+import { Course, useCourses } from "@/lib/dashboard/courses";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 
 export default function Courses() {
   const { token } = useAuth();
   const { data, isLoading, error } = useCourses(token);
+  // TODO: Filter data by student & get student data -> course instead of directly from course
+
+  const columns: ColumnDef<Course>[] = [
+    {
+      accessorKey: "code",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Code
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+    },
+    {
+      accessorKey: "name",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Name
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+    },
+    { accessorKey: "credits", header: "Credits" },
+  ];
 
   return (
     <main className="min-h-screen w-full p-4 pt-20 flex flex-col max-h-screen">
@@ -18,7 +55,7 @@ export default function Courses() {
           <Spinner size="32" />
         </>
       ) : (
-        <div>{JSON.stringify(data)}</div>
+        <DataTable columns={columns} data={data!.docs!} />
       )}
     </main>
   );
