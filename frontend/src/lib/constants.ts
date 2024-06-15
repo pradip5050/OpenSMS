0254fb5574c498756fc78f744fab2f9ecad21abb
dhzdhd <ragas5432100@gmail.com>
2024-06-15 20:28:24 +0530

    Migrate to using env instead of hardcoding url

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://127.0.0.1:3002";
+export const API_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 // Tauri (ADB devices)
 // export const API_URL = "http://10.0.2.2:3002";
