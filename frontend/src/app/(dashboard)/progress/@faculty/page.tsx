595d0dd7a6d509d72b7cc272466dcc331b61418f
dhzdhd <ragas5432100@gmail.com>
2024-07-01 14:03:08 +0530

    Revert "Add slider component and dummy faculty progress page"
    This reverts commit b4b567280d9d68a8b0222eb68107cd2391adcdce.

--- frontend/src/app/(dashboard)/progress/@faculty/page.tsx
+++ frontend/src/app/(dashboard)/progress/@faculty/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import { useAuth } from "@/components/AuthProvider";
-import { Combobox } from "@/components/dashboard/Combobox";
 import { DataTable } from "@/components/dashboard/DataTable";
 import GenericError from "@/components/GenericError";
 import SortButton from "@/components/SortButton";
 import { Badge } from "@/components/ui/badge";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Slider } from "@/components/ui/slider";
 import {
   Table,
   TableBody,
@@ -18,25 +16,11 @@ import {
 } from "@/components/ui/table";
 import { Course } from "@/lib/dashboard/courses";
 import { FacultyResponse, facultiesUrl } from "@/lib/dashboard/faculties";
-import {
-  Progress,
-  progressesUrl,
-  ProgressResponse,
-} from "@/lib/dashboard/progresses";
-import { StudentResponse, studentsUrl } from "@/lib/dashboard/students";
 import { useFetchCollection } from "@/lib/hooks";
 import { ColumnDef } from "@tanstack/react-table";
-import { useState } from "react";
 
 export default function FacultyCourses() {
   const { user, token } = useAuth();
-  const {
-    data: studentData,
-    isLoading: studentIsLoading,
-    error: studentError,
-  } = useFetchCollection<StudentResponse>(studentsUrl, token, {
-    depth: 2,
-  });
   const {
     data: facultyData,
     isLoading: facultyIsLoading,
@@ -45,54 +29,43 @@ export default function FacultyCourses() {
     depth: 2,
     where: { "user.email": { equals: user!.email } },
   });
-  const {
-    data: progressData,
-    isLoading: progressIsLoading,
-    error: progressError,
-  } = useFetchCollection<ProgressResponse>(progressesUrl, token, {
-    depth: 2,
-  });
-
-  const [value, setValue] = useState("");
 
   const faculty = facultyData?.docs?.at(0);
-  const student = studentData?.docs?.filter((student) => student.id === value);
   const courses = faculty?.courses;
-  const progresses = progressData?.docs;
-
-  const studentsOptions = studentData?.docs?.map((val) => {
-    return { value: val.id, label: val.user.name };
-  });
-
-  // ! Select student from combobox
-  // ! Then filter student course.subjects by faculty.subjects
-  // ! Then display a table of subjects with sliders
 
   // TODO: useMemo
-  const columns: ColumnDef<Progress>[] = [
+  const columns: ColumnDef<Course>[] = [
     {
-      accessorKey: "subject",
-      header: "Subject",
-      cell: ({ row }) => {
-        return row.original.subject.name;
+      accessorKey: "code",
+      header: ({ column }) => {
+        return <SortButton title="Code" column={column} />;
       },
     },
     {
-      accessorKey: "percent",
-      header: "Progress",
+      accessorKey: "name",
+      header: ({ column }) => {
+        return <SortButton title="Name" column={column} />;
+      },
+    },
+    { accessorKey: "credits", header: "Credits" },
+    { accessorKey: "duration", header: "Duration" },
+    {
+      accessorKey: "subjects",
+      header: "Subjects",
       cell: ({ row }) => {
-        return (
-          <div className="flex items-center">
-            <span className="w-12">{row.original.percent}%</span>
-            <Slider defaultValue={[row.original.percent]} max={100} step={1} />
-          </div>
-        );
+        return row.original.subjects.map((subject) => {
+          return (
+            <Badge className="mr-1 mb-1" key={subject.id}>
+              {subject.name}
+            </Badge>
+          );
+        });
       },
     },
   ];
 
-  const isLoading = facultyIsLoading || studentIsLoading || progressIsLoading;
-  const isError = !!facultyError || !!studentError || !!progressError;
+  const isLoading = facultyIsLoading;
+  const isError = !!facultyError;
 
   if (isLoading) {
     return (
@@ -137,16 +110,5 @@ export default function FacultyCourses() {
     return <GenericError variant="notImpl" />;
   }
 
-  return (
-    <div className="flex flex-col gap-3 overflow-y-auto">
-      <div className="flex justify-between">
-        <Combobox
-          options={studentsOptions!}
-          label="student"
-          state={{ value: value, setValue: setValue }}
-        />
-      </div>
-      <DataTable columns={columns} data={progresses!} />
-    </div>
-  );
+  return <DataTable columns={columns} data={courses!} />;
 }
