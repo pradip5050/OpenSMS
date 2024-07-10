b348e4a2ec5f8193dc99c42b131386a8fe8b54a9
dhzdhd <ragas5432100@gmail.com>
2024-07-10 11:26:36 +0530

    Add attendance and fee cronjobs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,11 +15,63 @@ const start = async () => {
     express: app,
     onInit: async () => {
       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
-
-      cron.schedule("* * * * * *", async () => {
-        console.log(await payload.count({ collection: "users" }));
-      });
     },
+  });
+
+  cron.schedule("0 0 0 * * *", async () => {
+    const students = await payload.find({ collection: "students" });
+
+    for (const student of students.docs) {
+      for (const course of (student as any)["courses"]) {
+        const attendanceCount = await payload.count({
+          collection: "attendances",
+          where: {
+            and: [
+              { course: { equals: course.id } },
+              { student: { equals: student.id } },
+            ],
+          },
+        });
+        const attendanceNotCreated = attendanceCount.totalDocs === 0;
+
+        if (attendanceNotCreated) {
+          await payload.create({
+            collection: "attendances",
+            data: {
+              date: new Date().toISOString(),
+              isPresent: false,
+              course: course.id,
+              student: student.id,
+            },
+          });
+        }
+      }
+    }
+  });
+
+  cron.schedule("0 0 0 * * *", async () => {
+    const fees = await payload.find({ collection: "fees" });
+    const curDate = new Date();
+
+    for (const fee of fees.docs) {
+      const dueDate = new Date((fee as any).dueDate);
+
+      if (curDate > dueDate) {
+        await payload.update({
+          collection: "fees",
+          data: {
+            id: fee.id,
+            paymentStatus: "delayed",
+          },
+          where: {
+            and: [
+              { id: { equals: fee.id } },
+              { paymentStatus: { equals: "unpaid" } },
+            ],
+          },
+        });
+      }
+    }
   });
 
   app.listen(3000);
